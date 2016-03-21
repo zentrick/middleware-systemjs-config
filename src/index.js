0300d183ef0ee161a24b8e645d2b1d4f29c94bdc
Laurent De Smet and Tim De Pauw <pair+laurent.desmet+tim.depauw@zentrick.com>
2016-03-21 17:29:18 +0100

    Support mode parameter for JS output

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import npmDependenciesToSystemConfig from 'npm-dependencies-to-system-config'
+import url from 'url'
 
 const DEFAULT_OPTIONS = {
   pkgRoot: process.cwd(),
@@ -12,16 +13,21 @@ export default (options) => {
   delete npmDependenciesToSystemConfigOptions.pkgRoot
 
   return (req, res, next) => {
+    const useJs = (url.parse(req.url, true).query.mode === 'js')
+
     npmDependenciesToSystemConfig(pkgRoot, npmDependenciesToSystemConfigOptions)
       .then((config) => {
         for (const pkg of Object.keys(config.packages).map((key) => config.packages[key])) {
           pkg.defaultExtension = false
         }
 
-        res.writeHead(200, {
-          'Content-Type': 'application/json'
-        })
-        res.end(JSON.stringify(config))
+        const json = JSON.stringify(config)
+        const [mimeType, body] = useJs
+          ? ['application/javascript', `System.config(${json})`]
+          : ['application/json', json]
+
+        res.writeHead(200, {'Content-Type': mimeType})
+        res.end(body)
       })
       .catch(next)
   }
